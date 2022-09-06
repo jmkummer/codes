e5f1c26eb7a8a689d7770aebdafdd683c1e05a84
jmkummer <junji.matsuguma@dell.com>
2022-09-06 11:49:10 +0900

    Update config.py

--- Lab07/config.py
+++ Lab07/config.py
@@ -1,6 +1,6 @@
 ecs_test_drive = {
     'ecs_endpoint_url' : 'https://object.ecstestdrive.com',
-    'ecs_access_key_id' : '132968072424510691@ecstestdrive.emc.com',
-    'ecs_secret_key' : '1I1MK5IU+uJotyAoYFqmiBczx8nphxLRkLY13+E6',
-    'ecs_bucket_name' : 'photo-album'
+    'ecs_access_key_id' : 'xxx@ecstestdrive.emc.com',
+    'ecs_secret_key' : 'xxxxx',
+    'ecs_bucket_name' : 'xxxxx'
 }
